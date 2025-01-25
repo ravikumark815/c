67e8bae6828be82d6aea040c1c1263e9dd6d29ae
Ravi Koothati <ravikumark815@gmail.com>
2025-01-25 13:37:04 -0500

    Check Prime

---
+++ Basic_Programs/CheckPrime.c
@@ -0,0 +1,21 @@
+/*
+Author: https://github.com/ravikumark815
+CheckPrime.c : Checks whether given number is a prime number
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for (int i=2; i<num; i++) {
+        if ((num%i) == 0) {
+            printf("%d is not a prime number\n", num);
+            return 0;
+        }
+    }
+    printf("%d is a prime number\n", num);
+    return 0;
+}
