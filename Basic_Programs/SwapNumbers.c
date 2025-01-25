5fc59ffa0ac8d74461e840490c2b6acded551b43
Ravi Koothati <ravikumark815@gmail.com>
2025-01-25 13:48:01 -0500

    SwapNumbers

---
+++ Basic_Programs/SwapNumbers.c
@@ -0,0 +1,31 @@
+/*
+Author: https://github.com/ravikumark815
+SwapNumbers.c : Swaps values of two integer variables
+*/
+
+#include <stdio.h>
+
+int swap(int *a, int *b) 
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+
+    return 0;
+}
+
+int main()
+{
+    int a, b;
+    
+    printf("Enter value of a: ");
+    scanf("%d", &a);
+    printf("Enter value of b: ");
+    scanf("%d", &b);
+
+    swap(&a, &b);
+
+    printf("After Swapping: a: %d, b: %d\n", a , b);
+    
+    return 0;
+}
