4a50a0529e99b2f4010d050c31fc9a9493f629bc
Ravi Koothati <ravikumark815@gmail.com>
2025-01-25 14:05:39 -0500

    Check Prime Number

--- Basic_Programs/CheckPrime.c
+++ Basic_Programs/CheckPrime.c
@@ -10,6 +10,12 @@ int main()
     int num;
     printf("Enter the number: ");
     scanf("%d", &num);
+    
+    if(num == 0 || num == 1) {
+        printf("%d is not a prime number\n", num);
+        return 0;
+    }
+
     for (int i=2; i<num; i++) {
         if ((num%i) == 0) {
             printf("%d is not a prime number\n", num);
